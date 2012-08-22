b6ff6ba8febbb9680ad277930c771a9b362e4b83
irony <cl@iteam.se>
2012-08-22 21:09:57 +0200

    polls

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.get('/', routes.index);
 app.get('/add', routes.add);
 app.post('/save', routes.save);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
