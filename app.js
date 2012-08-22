dddc9a7cef112b29b5ddf2867e3a1850320a6597
irony <cl@iteam.se>
2012-08-22 21:05:44 +0200

    polls

--- app.js
+++ app.js
@@ -20,16 +20,19 @@ app.configure(function(){
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
 
+
 app.get('/', routes.index);
+app.get('/add', routes.add);
+app.post('/save', routes.save);
 
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,22 @@
  * GET home page.
  */
 
+
+var polls = [];
+
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Polls', polls : polls });
+};
+
+
+
+exports.add = function(req, res){
+  res.render('add', { title: 'Add poll', polls : polls });
+};
+
+exports.save = function(req,res){
+
+  polls.push({id : polls.length, name : req.body.name, alternatives : req.body.alternatives.split(',')});
+  res.render('index', {title:'Thanks', polls:polls});
+
 };
